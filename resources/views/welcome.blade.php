43fb4d1ddab637e8e078ed010c27c2082c4ca929
max-ribeiro <max_ribeiro@live.com>
2023-09-30 18:39:41 -0300

    fix: ajuste user select

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -35,7 +35,7 @@
                 </p>
                 <p>Atualmente, busco aprimorar minhas habilidades para agregar valor na empresa onde trabalho,</p>
                 <p>não só nas entregas mas como tambem na qualidade do código e em melhorias do ambiente de desenvolvimento.</p>
-                <p>Além disso, desenvolvo alguns projetos pessoais utilizando <span class="language-icon"><i class="fa-brands fa-laravel"></i>Laravel/p></span>.
+                <p>Além disso, desenvolvo alguns projetos pessoais utilizando <span class="language-icon"><i class="fa-brands fa-laravel"></i>Laravel</span>.
                 <ul class="social-links">
                     <li><a href="https://linkedin.com/in/max-ribeiro" target="_blank"><i class="social-icon fab fa-linkedin"></i></a></li>
                     <li><a href="https://github.com/max-ribeiro" target="_blank"><i class="social-icon fab fa-github"></i></a></li>
